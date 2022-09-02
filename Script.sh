a016fa3e60675fd5a6d594e7685bdf33e9bbbca0
Amman Soomro <amman.soomro.as@gmail.com>
2022-09-02 22:05:28 +0500

    Added Extensions List

--- Script.sh
+++ Script.sh
@@ -1,2 +1,13 @@
-#!/bin/bash
-sudo apt-get Vitals
+ArcMenu
+Bluetooth Quick Connect
+Blur my Shell
+Custom Hot Corners - Extended
+Dash to Panel
+Desktop Icons
+Extension List
+Impatience
+OpenWeather
+Screenshot Tool
+Ubuntu Appindicators
+Vitals
+Window is Ready - Notification Remover
